47b38e9a7b1a2b3af98da1128e21c09fca948e0f
Jaunius Pinelis <JauniusPinelis@gmail.com>
2019-10-13 15:59:10 +0300

    Implementing Generic Repository pattern

---
+++ Square-Finder/SquareFinder.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SquareFinder.Infrastructure.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SquareFinder.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private PointContext _context;
+        private IMapper _mapper;
+        private DbSet<T> table = null;
+
+        public GenericRepository(PointContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+
+            table = _context.Set<T>();
+        }
+
+        public void Delete(int id)
+        {
+            var point = GetById(id);
+            table.Remove(point);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
